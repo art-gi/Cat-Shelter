7677d17bb408acc2e869b2d1fb40e1195a13cf18
art-gi <artgi@mail.bg>
2022-07-30 15:36:22 +0300

    add homecontroller

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const PORT = 5000;
+
+const homeController = require('./contolers/home.js');
 
 const requestListener = (req, res) => {
-res.write(200,'Hello');
-res.end();
+
+    switch (req.url) {
+        case '/':
+            homeController(req, res);
+            break;
+        /*  case '/addBreed':
+             res.write('Add breed');
+             res.end();
+             break;
+         case '/addCat':
+             res.write('Add cat');
+             res.end();
+             break;
+         case '/editCat':
+             res.write('Edit cat');
+             res.end();
+             break;
+         case '/addCat':
+             res.write('Add cat');
+             res.end();
+             break; */
+
+        default:
+            res.write('404 page');
+            res.end();
+    }
+
 };
 
-const server = http.createServer(requestListener)
+const server = http.createServer(requestListener);
 
-server.listen(5000)
+server.listen(PORT, console.log(`Server is running on port ${PORT}...`));
